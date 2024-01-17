43fd9be4dd8ee7cfa00d7caab72a5589555f976e
offa <bm-dev@yandex.com>
2024-01-17 16:30:15 +0100

    Format with ruff (#47)

--- generate_url.py
+++ generate_url.py
@@ -18,50 +18,62 @@
 import argparse
 import configparser
 
-CONFIG_FILE = 'jenkins.conf'
-__version__ = '0.1.0'
+CONFIG_FILE = "jenkins.conf"
+__version__ = "0.1.0"
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(prog="generate_url",
-                                     description='Generate Git Push Notification URLs for Jenkins.')
-    required = parser.add_argument_group('arguments')
-    required.add_argument('--jenkins',
-                          '-j',
-                          type=str,
-                          help='Jenkins URL (https://<url>[:<port>])',
-                          required=True)
-    required.add_argument('--environment',
-                          '-e',
-                          type=str,
-                          help='Jenkins Environment (jenkins.conf)')
-    required.add_argument('--all',
-                          '-a',
-                          action='store_true',
-                          help='All Jenkins Environments (jenkins.conf)')
-    required.add_argument('--token', '-t', type=str, help='Access token', required=True)
-    required.add_argument('repo',
-                          type=str,
-                          nargs="+",
-                          help='Git Repository URL (ssh://<url> or https://<url>)')
-    parser.add_argument('--quiet', '-q', action='store_true', help='Prints URL only')
-    parser.add_argument('--version',
-                        '-v',
-                        action='version',
-                        version=f"%(prog)s {__version__}",
-                        help='Shows the program version')
+    parser = argparse.ArgumentParser(
+        prog="generate_url",
+        description="Generate Git Push Notification URLs for Jenkins.",
+    )
+    required = parser.add_argument_group("arguments")
+    required.add_argument(
+        "--jenkins",
+        "-j",
+        type=str,
+        help="Jenkins URL (https://<url>[:<port>])",
+        required=True,
+    )
+    required.add_argument(
+        "--environment", "-e", type=str, help="Jenkins Environment (jenkins.conf)"
+    )
+    required.add_argument(
+        "--all",
+        "-a",
+        action="store_true",
+        help="All Jenkins Environments (jenkins.conf)",
+    )
+    required.add_argument("--token", "-t", type=str, help="Access token", required=True)
+    required.add_argument(
+        "repo",
+        type=str,
+        nargs="+",
+        help="Git Repository URL (ssh://<url> or https://<url>)",
+    )
+    parser.add_argument("--quiet", "-q", action="store_true", help="Prints URL only")
+    parser.add_argument(
+        "--version",
+        "-v",
+        action="version",
+        version=f"%(prog)s {__version__}",
+        help="Shows the program version",
+    )
     return parser.parse_args()
 
 
 def load_config():
     cfg = configparser.ConfigParser()
     cfg.read(CONFIG_FILE)
-    return cfg['instances']
+    return cfg["instances"]
 
 
 def generate_url(repo, jenkins, token):
-    url_prefix = "" if jenkins.startswith('https://') or jenkins.startswith(
-        "http://") else "https://"
+    url_prefix = (
+        ""
+        if jenkins.startswith("https://") or jenkins.startswith("http://")
+        else "https://"
+    )
     return f"{url_prefix}{jenkins}/git/notifyCommit?url={repo}&token={token}"
 
 
@@ -69,7 +81,7 @@ def print_url(url, name, quiet):
     if quiet:
         print(f"{url}")
     else:
-        prefix = f" * {name}:\t " if name else 'Push Notification URL:\n\n'
+        prefix = f" * {name}:\t " if name else "Push Notification URL:\n\n"
         print(f"{prefix}{url}")
 
 
@@ -86,10 +98,10 @@ def main():
         instances = [(None, "<URL>:<PORT>")]
 
     for repo in args.repo:
-        for (name, url) in instances:
+        for name, url in instances:
             notification_url = generate_url(repo, url, args.token)
             print_url(notification_url, name, args.quiet)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
